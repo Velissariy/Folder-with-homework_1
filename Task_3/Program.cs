7224052786d45133758cc90e02c483db22bf4f12
Juno <polya.alexei@yandex.ru>
2023-02-13 17:54:14 +0300

    Task_3  complited

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -4,162 +4,17 @@
 // -3 -> нет
 // 7 -> нет
 
+Console.WriteLine("Введите число");
 
-System.Console.WriteLine("Введите число");
-double numberA = Convert.ToInt32(System.Console.ReadLine());
-int numberB = Convert.ToInt32(System.Console.ReadLine()); 
-double x = Convert.ToInt32(0);
-double y = Convert.ToInt32(0);
+int numberA ;
 
-numberB = numberA;
+numberA = int.Parse(Console.ReadLine());
 
-x = numberA / 2;
-
-y = numberB / 2;
-// //y = x;
-
-//  double numberA = numberA;
-//     int b = 0;
-//   b = (int)numberA;
-//   b = numberA / 2;
-    
-//    y = x - b;
-
-// Console.Write(b);
-Console.Write(x);
-Console.Write(y);
-
-
-
-// using System;
- 
-// public class Example
-// {
-//     public static void Main()
-//     {
-//         bool b = true;
-//         int i = Convert.ToInt32(b);
-//         Console.WriteLine(i);    // 1
- 
-//         b = false;
-//         i = Convert.ToInt32(b);
-//         Console.WriteLine(i);    // 0
-//     }
-// }
-
-
-
-
-
-
-// //int x = Convert.ToInt32(0);
-
-//     {
-//         bool b = true;
-//         int i = Convert.ToInt32(b);
-//         //Console.WriteLine(i);    // 1
- 
-//         b = false;
-//         i = Convert.ToInt32(b);
-//         //Console.WriteLine(i);    // 0
-//     }
-
-// if (( numberA/2)%0) 
-// {
-//   System.Console.Write("Да");
-// }
-// else 
-//    System.Console.Write("Нет");
-
-
-
-// bool t = true;
-// int i = t ? 1 : 0;
-// Console.Write(i); // 1
-
-// bool f = false;
-// int y = Convert.ToInt32(f);
-// Console.WriteLine(y); // 0
-
-// System.Console.Write("Да");
-
-// class Program
-// {
-//     static void Main()
-//     {
-// 	// Example bool is true.
-// 	bool t = true;
-
-// 	// A.
-// 	// Convert bool to int.
-// 	int i = t ? 1 : 0;
-// 	Console.WriteLine(i); // 1
-
-// 	// Example bool is false.
-// 	bool f = false;
-
-// 	// B.
-// 	// Convert bool to int.
-// 	int y = Convert.ToInt32(f);
-// 	Console.WriteLine(y); // 0
-//     }
-// }
-
-// Output
-
-//1
-//0
-
-// using System;
- 
-// using System.Collections.Generic;
- 
-// using System.Linq;
- 
-// using System.Text;
- 
- 
- 
-// namespace check1
- 
-// {
- 
-//     class Program
- 
-//     {
- 
-//         static void Main(string[] args)
- 
-//         {
- 
-//             int i;
- 
-//             Console.Write("Введите число : ");
- 
-//             i = int.Parse(Console.ReadLine());
- 
-//             if (i % 2 == 0)
- 
-//             {
- 
-//                 Console.Write("Введенное число является четным");
- 
-//                 Console.Read();
- 
-//             }
- 
-//             else
- 
-//             {
- 
-//                 Console.Write("Введенное число нечетное");
- 
-//                 Console.Read();
- 
-//             }
- 
-//         }
- 
-//     }
- 
-// }
+if (numberA % 2 == 0)
+  {
+   Console.Write("Да");
+  }
+else
+  {
+   Console.Write("Нет");
+  }
